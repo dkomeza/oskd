59f8b04eb06c2b4827ed725b7d47f92ac3e74120
Dawid Komęza <dawid.komeza@gmail.com>
2023-12-25 12:31:05 +0100

    ✨ Update the install script

--- lib/install_dependencies.sh
+++ lib/install_dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Install dependencies
+echo "Installing dependencies..."
 git clone https://github.com/Bodmer/TFT_eSPI.git
+echo "Dependencies installed!"
+
+# Copy the user_setup.h file to the TFT_eSPI library folder
+echo "Copying User_Setup.h file to TFT_eSPI library folder..."
+cp User_Setup.h TFT_eSPI/User_Setup.h
+echo "User_Setup.h file copied!"
