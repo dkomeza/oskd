0b16f8c94eb1803077dda54f64d8e66e74f5ef94
Dawid Komęza <dawid.komeza@gmail.com>
2024-01-21 19:48:05 +0100

    🔥 Clean up the main file

--- src/main.cpp
+++ src/main.cpp
@@ -16,36 +16,10 @@ bool rising = true;
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Starting...");
-
-    // connection::setupOTA();
-    screen::setup();
-
-    button.setLongPressTime(600);
-
-    button.onClick([]()
-                   { data::gear--; });
-
-    button.onLongPress([]()
-                       { data::power -= 13; },
-                       50);
-
-    button2.onClick([]()
-                    { data::gear++; });
-
-    button2.onLongPress([]()
-                        { data::power += 13; },
-                        50);
-
-    Serial.println("Started!");
+    
 }
 
 void loop()
 {
-    connection::loop();
-
-    button.update();
-    button2.update();
-
-    dashboard::update();
+    
 }
