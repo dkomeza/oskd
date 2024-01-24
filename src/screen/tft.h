a9a08bcf61d578eeb5252e64742a7c9350f56e8c
Dawid Komęza <dawid.komeza@gmail.com>
2024-01-24 23:12:47 +0100

    🔥 Remove unused font files & rename remaining

--- src/screen/tft.h
+++ src/screen/tft.h
@@ -3,16 +3,14 @@
 #include <TFT_eSPI.h>
 #include <Arduino.h>
 
-#include "fonts/Font12.h"
-#include "fonts/Font20.h"
-#include "fonts/Font26.h"
+#include "fonts/FONT_S.h"
+#include "fonts/FONT_SM.h"
+#include "fonts/FONT_M.h"
 #include "fonts/FONT_L.h"
-#include "fonts/Font96.h"
-#include "fonts/binaryttf.h"
+#include "fonts/FONT_XL.h"
 
 typedef uint16_t Color;
 
-#define FONT_BINARY binaryttf
 #define FONT_S Lato12
 #define FONT_SM Lato20
 #define FONT_M Lato26
